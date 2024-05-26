7143e147357c923654e34891a4a30410c0514efb
DRIX10's alternative account <126459477+reef-bot@users.noreply.github.com>
2024-05-26 07:38:49 +0530

    Added: `config/config.js`

---
+++ config/config.js
@@ -0,0 +1,12 @@
+// config.js
+
+const config = {
+  discordToken: "YOUR_DISCORD_BOT_TOKEN",
+  googleCloudApiKey: "YOUR_GOOGLE_CLOUD_API_KEY",
+  mongoURI: "YOUR_MONGODB_URI",
+  herokuAppUrl: "YOUR_HEROKU_APP_URL",
+  oauthClientId: "YOUR_OAUTH_CLIENT_ID",
+  oauthClientSecret: "YOUR_OAUTH_CLIENT_SECRET"
+};
+
+module.exports = config;
